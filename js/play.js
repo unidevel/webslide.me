e4c1aef1ef80120ab1a39c6ac7a4476785a15812
Christoph Martens <christoph@compaq615.(none)>
2011-07-11 02:08:04 +0200

    Done some refactoring of webslide.me player. Remote still needs to be refactored

--- js/play.js
+++ js/play.js
@@ -3,6 +3,9 @@
 
 webslide.me.player = function(remote) {
 
+	this.__ui = {};
+	this.__slideCache = [];
+
 	this.__init();
 
 };
@@ -10,63 +13,304 @@ webslide.me.player = function(remote) {
 
 webslide.me.player.prototype = {
 
+
+
+	go: function(direction) {
+
+		if (!direction) return;
+
+		var steps = document.getElementsByClassName('animation-step');
+
+
+		if (direction == 'next') {
+
+			// Animate a Step, not a Slide
+			if (steps && steps.length) {
+
+				for (var s = 0, l = steps.length; s < l; s++) {
+					var step = steps[s];
+					if (
+						this.__activeSlide === step.parentNode
+						&& step.style.opacity != 1
+					) {
+						step.style.opacity = 1;
+
+						// Skip navigation to next Slide
+						return;
+					}
+				}
+
+			}
+
+			// Animate to next Slide
+			var currentSlideIndex = this.__activeSlideIndex,
+				nextSlideIndex = currentSlideIndex;
+
+			// Yep, the next slide is also available in the slideCache
+			if (this.__slideCache[currentSlideIndex + 1]) {
+				nextSlideIndex = currentSlideIndex + 1;
+			}
+
+			if (currentSlideIndex != nextSlideIndex) {
+
+				var currentSlide = this.__activeSlide,
+					nextSlide = this.__slideCache[nextSlideIndex];
+
+				currentSlide.removeAttribute('aria-selected');
+				currentSlide.removeAttribute('data-next');
+				currentSlide.setAttribute('data-prev', 'true');
+
+				// API for playback capabilities (e.g. video / audio)
+				if (currentSlide.hasAttribute('data-onunload') {
+					window[currentSlide.getAttribute('data-onunload')].call(window, currentSlide);
+				}
+
+				nextSlide.removeAttribute('data-next');
+				nextSlide.setAttribute('aria-selected', 'true');
+
+				// API for playback capabilities (e.g. video / audio)
+				if (nextSlide.hasAttribute('data-onload') {
+					window[nextSlide.getAttribute('data-onload')].call(window, nextSlide);
+				}
+
+
+				// Update our cached activeSlide now
+				this.__activeSlide = nextSlide;
+				this.__activeSlideIndex = nextSlideIndex;
+
+				window.location.hash = nextSlide.id;
+				this.__ui.pagenumber.innerText = nextSlide.id;
+
+			}
+
+		} else if (direction == 'prev') {
+
+			// Animate a Step, not a Slide
+			if (steps && steps.length) {
+
+				for (var s = (steps.length - 1); s >= 0; s--) {
+					var step = steps[s];
+					if (
+						this.__activeSlide == step.parentNode
+						&& step.style.opacity == 1
+					) {
+						step.style.opacity = 0;
+
+						// Skip navigation to previous Slide
+						return;
+					}
+				}
+
+			}
+
+			// Animate to previous Slide
+			var currentSlideIndex = this.__activeSlideIndex,
+				prevSlideIndex = currentSlideIndex;
+
+			// Yep, the previous slide is also available in the slideCache
+			if (this.__slideCache[currentSlideIndex - 1]) {
+				prevSlideIndex = currentSlideIndex - 1;
+			}
+
+			if (currentSlideIndex != prevSlideIndex) {
+
+				var currentSlide = this.__activeSlide,
+					prevSlide = this.__slideCache[prevSlideIndex];
+
+				currentSlide.removeAttribute('aria-selected');
+				currentSlide.removeAttribute('data-prev');
+				currentSlide.setAttribute('data-next', 'true');
+
+				// API for playback capabilities (e.g. video / audio)
+				if (currentSlide.hasAttribute('data-onunload') {
+					window[currentSlide.getAttribute('data-onunload')].call(window, currentSlide);
+				}
+
+				prevSlide.removeAttribute('data-prev');
+				prevSlide.setAttribute('aria-selected', 'true');
+
+				// API for playback capabilities (e.g. video / audio)
+				if (prevSlide.hasAttribute('data-onload') {
+					window[prevSlide.getAttribute('data-onload')].call(window, prevSlide);
+				}
+
+
+				// Update our cached activeSlide now
+				this.__activeSlide = prevSlide;
+				this.__activeSlideIndex = prevSlideIndex;
+
+				window.location.hash = prevSlide.id;
+				this.__ui.pagenumber.innerText = prevSlide.id;
+
+			}
+
+		}
+
+	},
+
+
+
 	__init: function() {
 
 		var title = document.getElementsByTagName('title')[0].textContent,
 			slides = document.getElementsByTagName('section');
 
+
+		// Update the document title
 		if (!title && document.title) {
 			title = document.title
 		} else {
-			'Please update'
+			title = 'webslide.me | Please update your old Browser.';
 		}
 
-		// IE fix, because IE doesn't know textContent =/
-		title= (title) ? title : ((document.title) ? document.title : 'Please use a more up-to-date Web Browser!');
-
-		// cache the elements
-		for (var i=0;i<slides.length;i++){
-			slides[i].setAttribute('id','slide-'+(i + 1));
-			slides[i].setAttribute('data-next','true'); // prepare animation positioning
-			this.slides.push(slides[i]);
+		// Update the slide cache
+		if (slides && slides.length){
+			for (var s = 0, l = slides.length; s < l; s++) {
+				slides[s].id = 'slide-' + (s + 1);
+				if (s > 0) {
+					slides[s].setAttribute('data-next', 'true');
+				}
+				this.__slideCache.push(slides[i]);
+			}
 		}
 
-		// create the footer with information
-		var footer = document.createElement('footer');
-		footer.id = 'footer';
-		footer.innerHTML = '<div class="title">'+title+'</div>'+'<div class="index"><span id="pagenumber">1</span> / ' + slides.length +'</div>';
-		document.getElementsByTagName('body')[0].appendChild(footer);
+		// footer
+		this.__ui.footer = document.createElement('footer');
+		this.__ui.footer.id = 'footer';
+		this.__ui.footer.innerHTML = '<div class="title">' + title + '</div>';
 
-		// set first slide to active
-		this.set('active',this.slides[0]);
+		var footerIndex = document.createElement('div');
+		footerIndex.className = 'index';
 
-		// attach the events
-		this.attach_events();
+		this.__ui.pagenumber = document.createElement('span');
+		this.__ui.pagenumber.id = 'pagenumber';
+		footerIndex.appendChild(this.__ui.pagenumber);
 
-		// finally activate the remote control
-		if(remote){
-			this.remote.init();
+		// Default navigation
+		this.__ui.navi = document.createElement('div');
+		this.__ui.navi.id = 'navigation';
+
+
+		// previous button
+		this.__ui.prevButton = document.createElement('button');
+		this.__ui.prevButton.title = 'previous slide';
+		this.__ui.prevButton.setAttribute('accesskey', this.settings.accesskeys.prev);
+		this.__ui.prevButton.innerText = '&lt;';
+		this.__ui.prevButton.onclick = function(){
+			that.go('prev');
+		};
+
+
+		// next button
+		this.__ui.nextButton = document.createElement('button');
+		this.__ui.nextButton.title = 'next slide';
+		this.__ui.nextButton.setAttribute('accesskey', this.settings.accesskeys.next);
+		this.__ui.nextButton.innerText = '&gt;';
+		this.__ui.nextButton.onclick = function(){
+			that.go('next');
+		};
+
+
+		this.__ui.footer.appendChild(footerIndex);
+		this.__ui.navi.appendChild(this.__ui.backwardButton);
+		this.__ui.navi.appendChild(this.__ui.forwardButton);
+		this.__ui.footer.appendChild(this.__ui.navi);
+		document.body.appendChild(this.__ui.footer);		
+
+		// Initial Setup
+		this.__activeSlide = this.__slideCache[0];
+		this.__activeSlideIndex = 0;
+		this.__attachEvents();
+
+		if (remote) {
+			this.initRemote();
 		}
-
-
-
-
-
-
 
 	},
 
-	methods: (function(){
+	__handleEvent: function(event) {
 
+		// Stupidz called thiz
+		if (!event) return;
+
+		// Fixes touch/drag issues on Webkit Mobile
+		if (typeof (event.preventDefault) == 'function') {
+			event.preventDefault();
+		}
+
+		// Use the first recognized touch on MultiTouch environments
+		if (event.touches) {
+			event = event.touches[0];
+		}
+
+		// Mouse Click
+		if (event.clientX) {
+
+			// Skip on Right or Middle Mouse Button
+			if (event.button && event.button !== 0) {
+				return;
+			}
+
+			if (event.clientX < window.innerWidth / 2) {
+				this.go('next');
+			} else {
+				this.go('prev');
+			}
+
+		// Keyboard
+		} else if (event.keyCode) {
+
+			// PageDown
+			if (event.keyCode == 34) {
+				this.go('next');
+			// PageUp
+			} else if (event.keyCode == 33) {
+				this.go('prev');
+			}
+
+		}
+
+		// Avoid Event Bubbling
+		return false;
+
+	},
+
+	__attachEvents: function() {
+
+
+		var that = this,
+			slides = this.__slideCache,
+			playMethods = this.lib.playMethods;
+
+		// Only attach events if features were detected the right way
+		if (slides && slides.length && playMethods && playMethods.length) {
+
+			for (var s = 0, l = slides.length; s < l; s++) {
+
+				for (var p = 0, pl = playMethods.length; p < pl; p++) {
+					this.lib.addEvent(slides[s], playMethods[p], function(event){
+						that.__handleEvent.apply(event); 
+					});
+				}
+
+			}
+
+		}
+
+	},
+
+	lib: (function(){
+
+		// Event Method
 		var eventMethod;
 
 		if (window.addEventListener) {
-			eventMethod = function(element, callback) {
-				window.addEventListener(element, callback, true);
+			eventMethod = function(element, type, callback) {
+				element.addEventListener(type, callback, true);
 			};
 		} else if (window.attachEvent) {
 			eventMethod = function(element, callback) {
-				window.attachEvent(element, callback);
+				element.attachEvent(type, callback);
 			};
 		} else {
 			eventMethod = function(element, callback) {
@@ -74,8 +318,7 @@ webslide.me.player.prototype = {
 			};
 		}
 
-
-
+		// Play Methods
 		var playMethod;
 		this.__playMethods = '';
 
@@ -112,8 +355,8 @@ webslide.me.player.prototype = {
 
 		// public API
 		return {
-			event: eventMethod,
-			play: this.__playMethods.trim().split(' ')
+			addEvent: eventMethod,
+			playMethods: this.__playMethods.trim().split(' ')
 		};
 
 
