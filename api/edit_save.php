62f2d16519122eb3f04570edf066aa5962ed3403
Christoph Martens <christoph@martens.ms>
2011-08-10 12:03:15 +0200

    Blob upload is now ready.

--- api/edit_save.php
+++ api/edit_save.php
@@ -6,61 +6,64 @@
  * @meta = {json}
  * @user = "string"
  * @skey = "string"
- * @webslide = <html>
+ * @blob = <html> -> can be post or blob data
  */
 
 
-if(
+if ($_FILES['blob']) {
+	$req['webslide'] = file_get_contents($_FILES['blob']['tmp_name']);
+} else {
+	$req['webslide'] = $req['blob'];
+}
+
+if (
 	$api['valid_session'] && !in_array(basename($req['file']), $db['protected'])
 ){
 
-	var_dump($_FILES);
-	exit;
+	if (!empty($req['webslide'])) {
 
-	if (!empty($req['blob'])) {
-		// TODO: encode webslide to hexstring and decode it =/
-		// $req['webslide']=eregi_replace("_amp_","&",$req['webslide']);
+		$dir['home'] = $dir['db']."/".$req['user'];
+		$req['file'] = strtolower($req['file']);
+		$webslide['html'] = $dir['home']."/".$req['file']; // transferred with .html
+		$webslide['json'] = $dir['home']."/".eregi_replace(".html",".json",$req['file']);
 
-		$dir['home']=$dir['db']."/".$req['user'];
-		$req['file']=strtolower($req['file']);
-		$webslide['html']=$dir['home']."/".$req['file']; // transferred with .html
-		$webslide['json']=$dir['home']."/".eregi_replace(".html",".json",$req['file']);
-
-		if(!is_dir(dirname($webslide['html']))){
+		if (!is_dir(dirname($webslide['html']))) {
 			// 1=x, 2=w, 4=r
 			@mkdir(dirname($webslide['html']),0775);
 		}
 
-		$meta=json_decode($req['meta'],true);
-		foreach($meta as $key => $val){
-			if($key!='theme'){
-				if(empty($val)){
-					$meta[$key]='No '.$key;
+		$meta = json_decode($req['meta'], true);
+		foreach ($meta as $key => $val) {
+			if ($key!='theme') {
+				if (empty($val)) {
+					$meta[$key] = 'No '.$key;
 				}
 			}
 		}
-		$req['meta']=json_encode($meta,true);
+		$req['meta'] = json_encode($meta, true);
 
-		if(
-			file_put_contents($webslide['html'],$req['webslide'])
-			&& file_put_contents($webslide['json'],$req['meta'])
+
+		if (
+			file_put_contents($webslide['html'], $req['webslide'])
+			&& file_put_contents($webslide['json'], $req['meta'])
 		){
 			header('HTTP/1.0 200 OK');
 			// only echo filename if download is disabled
-			if($req['download']!='yes'){
+			if ($req['download'] !== 'yes') {
 				echo basename($webslide['html']);
 				exit;
-			}else{ // reroute to download site
+			} else { // reroute to download site
 				//header("Location: http://".$req['user'].".webslide.me/api/download/".basename($webslide['html']));
 				echo "http://".$req['user'].".".$domain."/api/download/".basename($webslide['html']);
 				exit;
 			}
-		}else{
+		} else {
 			header('HTTP/1.0 500 Internal Server Error');
+			echo "Something went wrong while storing the file to the database.";
 			exit;
 		}
 	}
-}else{
+} else {
 	header('HTTP/1.0 403 Forbidden');
 	exit;
 }
--- api/webslides.php
+++ api/webslides.php
@@ -3,23 +3,18 @@
 /*
  * @user = "string"
  */
-if(!empty($_POST)){
-	foreach($_POST as $key => $val){
-		$req[$key]=$val;
-	}
-}
 
-$tmp=explode(".",$_SERVER['HTTP_HOST']);
-$req['user']=(($_REQUEST['user'])?$_REQUEST['user']:(($tmp[0]!='webslide' && $tmp[0]!='www')?$tmp[0]:(($_COOKIE['user'])?$_COOKIE['user']:'demo')));
+if (!empty($req['user'])) {
+	
+	$dir['home'] = $dir['db']."/".$req['user'];
 
-if(!empty($req['user'])){
-	$dir['home']=$dir['db']."/".$req['user'];
+	if (is_dir($dir['home'])) {
+		$webslides = array();
+		$dh = opendir($dir['home']);
+		while (($file = readdir($dh)) !== false) {
 
-	if(is_dir($dir['home'])){
-		$webslides=array();
-		$dh=opendir($dir['home']);
-		while(($file=readdir($dh))!==false){
-			if(is_file($dir['home']."/".$file) && preg_match("~(.*)\.html~Uis",$file)){
+			if (is_file($dir['home']."/".$file) && preg_match("~(.*)\.html~Uis",$file)) {
+
 				$meta=json_decode(file_get_contents($dir['home']."/".eregi_replace(".html",".json",$file)),true);
 
 				$webslides[]=array(
@@ -35,7 +30,8 @@ if(!empty($req['user'])){
 	header('HTTP/1.0 200 OK');
 	echo json_encode($webslides);
 	exit;
-}else{
+
+} else {
 	header('HTTP/1.0 404 Not Found');
 	exit;
 }
